6c8e04133ad5c5c0ae644c536c641dfbc3f68042
kushal <kushalbhalla14@gmail.com>
2020-09-11 11:00:09 +0530

    Added recipe image feature.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets, mixins
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import viewsets, mixins, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
@@ -54,34 +56,31 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Return appropriate serializer class"""
         if self.action == 'retrieve':
             return serializers.RecipeDetailSerializer
+
+        elif self.action=='upload_image':
+            return serializers.RecipeImageSerializer
         return self.serializer_class
 
     def perform_create(self, serializer):
         """Create a new recipe"""
         serializer.save(user=self.request.user)
-# class BaseRecipeAttrViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
-#     """Base viewset for user owned recipe attributes"""
-#     authentication_classes = (TokenAuthentication,)
-#     permission_classes = (IsAuthenticated,)
-#
-#     def get_queryset(self):
-#         """Return objects for the current authenticated uer only"""
-#         return self.queryset.filter(user=self.request.user.id).order_by('-name')
-#
-#     def perform_create(self, serializer):
-#         """Create a new object"""
-#         serializer.save(user=self.request.user)
-#
-#
-# class TagViewSet(BaseRecipeAttrViewSet):
-#     """Manage tags in the database"""
-#
-#     queryset = Tag.objects.all()
-#     serializer_class = serializers.TagSerializer
-#
-#
-# class IngredientViewSet(BaseRecipeAttrViewSet):
-#     """Manage ingrdient in the database"""
-#
-#     queryset = Ingredient.objects.all()
-#     serializer_class = serializers.IngredientSerializer
+
+    @action(methods=['POST'], detail=True, url_path='upload_image')
+    def upload_image(self,request, pk=None):
+        """Upload an image for a recipe"""
+        recipe = self.get_object()
+        serializer = self.get_serializer(
+            recipe, data=request.data
+        )
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+            serializer.data,
+            status=status.HTTP_200_OK
+            )
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
